1abbf0fd4345012d8177dd21aea9e9c66e36481d
shreyasutar24 <shreyasutar2408@gmail.com>
2026-07-08 13:03:59 +0530

    home page

---
+++ src/app/gallary/gallary.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallary',
+  imports: [],
+  templateUrl: './gallary.html',
+  styleUrl: './gallary.css',
+})
+export class Gallary {}
