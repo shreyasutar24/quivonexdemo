623259b609b47cc36f2e7ffc6bb5a60064df5b05
Gayatridhanave28 <gayatridhanave1@gmail.com>
2026-07-08 13:09:56 +0530

    bootstrap

---
+++ src/app/demoocomp/demoocomp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demoocomp',
+  imports: [],
+  templateUrl: './demoocomp.html',
+  styleUrl: './demoocomp.css',
+})
+export class Demoocomp {
+
+}
