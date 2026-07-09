0080e6c17cbde91589202645715c85b23fffde75
Gayatridhanave28 <gayatridhanave1@gmail.com>
2026-07-09 12:41:13 +0530

    rreem

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -16,10 +16,25 @@ export class Contact {
   contactForm!: FormGroup;
 
   contactInfo = [
-    { title: 'Email Us', value: 'quivonexsolutions@gmail.com', icon: 'ti-mail' },
-    { title: 'Call Us', value: '(+91) 9226859922 (Toll Free)', icon: 'ti-phone' },
-    { title: 'Visit Us', value: '402,Shri Datta Lakxmi Banglow,Shri Krishna Colony Dhangarwadi Old MIDC Kodoli, Satara, Maharashtra 415004', icon: 'ti-map-pin' }
-  ];
+  { 
+    title: 'Email Us', 
+    value: 'quivonexsolutions@gmail.com', 
+    icon: 'ti-mail',
+    link: 'mailto:quivonexsolutions@gmail.com'
+  },
+  { 
+    title: 'Call Us', 
+    value: '(+91) 9226859922 (Toll Free)', 
+    icon: 'ti-phone',
+    link: 'tel:+919226859922'
+  },
+  { 
+    title: 'Visit Us', 
+    value: '402, Shri Datta Lakxmi Banglow, Shri Krishna Colony Dhangarwadi Old MIDC Kodoli, Satara, Maharashtra 415004', 
+    icon: 'ti-map-pin',
+    link: 'google.com/maps?ll=17.673694,74.029008&z=16&t=m&hl=en&gl=IN&mapclient=embed&q=17°40%2725.3"N+74°01%2753.7"E+17.673694,+74.031583@17.6736944,74.0315833'
+  }
+];
 
   socials: { icon: string; url: string }[] = [
     { icon: 'ti-brand-facebook', url: 'https://www.facebook.com/people/Quivonex-Solutions/61588563381546/' },
