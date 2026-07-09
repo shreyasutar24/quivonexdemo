3c23facfb828b23e9e357424a2fa7f4c0c7be9c2
Gayatridhanave28 <gayatridhanave1@gmail.com>
2026-07-09 10:39:49 +0530

    new comp

---
+++ src/app/tempooo/tempooo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tempooo',
+  imports: [],
+  templateUrl: './tempooo.html',
+  styleUrl: './tempooo.css',
+})
+export class Tempooo {}
